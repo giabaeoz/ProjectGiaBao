caebc2898d0f4d595247a72bdc24e097165a0cc8
giabaeoz <ngobaokakaka@gmail.com>
2026-02-03 20:20:11 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function showAlert() {
-    alert("Chào mừng bạn đến với trang web Đại học Trà Vinh!");
+    alert("Chào mừng bạn đến với website giới thiệu Đại học Trà Vinh!");
+}
+
+function openTVU() {
+    window.open("https://tvu.edu.vn", "_blank");
+}
+function openTTSV() {
+    window.open("https://ttsv.tvu.edu.vn", "_blank");
 }
